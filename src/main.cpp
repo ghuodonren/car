92aafd3a697e33fafa9fdf3af40028f48101ad4a
ghuodonren <q3578997790@gmail.com>
2026-03-22 21:29:28 +0800

    nine

--- src/main.cpp
+++ src/main.cpp
@@ -15,11 +15,11 @@ void handleLostState()  //调整小车偏离轨道的补救函数
     int rightSum = prev_s2 + prev_s3;   //右边总计
 
     if(leftSum > rightSum){
-        SwerveLeftTurn(255);
+        SwerveLeftTurn(200);
     }else if(leftSum < rightSum){
-        SwerveRightTurn(255);
+        SwerveRightTurn(200);
     }else{
-        SwerveLeftTurn(150);
+        SwerveReverse(150);
     }
 }
 
@@ -46,44 +46,46 @@ void loop()
     int state = (s0 << 3) + (s1 << 2) + (s2 << 1) + s3; //记忆四个红外模块状态值
 
     if(state == 6){
-        SwerveForward(255); //0110,小车保持直行
+        SwerveForward(240); //0110,小车保持直行
     }else if(state == 2 || state == 3){
-        SwerveRightTurn(200); //0010、0011，小车小幅度右转
+        SwerveRightTurn(160); //0010、0011，小车小幅度右转
     }else if(state == 4 || state == 12){
-        SwerveLeftTurn(200); //0100、1100，小车小幅度左转
+        SwerveLeftTurn(160); //0100、1100，小车小幅度左转
     }else if(state == 1){
-        SwerveRightTurn(255); //0001，小车严重偏右，大幅度右转
+        SwerveRightTurn(240); //0001，小车严重偏右，大幅度右转
     }else if(state == 8){
-        SwerveLeftTurn(255); //1000，小车严重偏左，大幅度左转
+        SwerveLeftTurn(240); //1000，小车严重偏左，大幅度左转
     }else if(state == 0){
         handleLostState(); //0000，小车偏移轨道，使用补救函数
     }else if(state == 15){
-        SwerveForward(200); //1111，小车可能遇到十字路口，保持直行
+        SwerveForward(160); //1111，小车可能遇到十字路口，保持直行
     }else{
         //额外情况所作出的调整
         int leftSum = s0 + s1;
         int rightSum = s2 + s3;
 
         if(leftSum > rightSum){ //小车偏左
-            SwerveLeftTurn(180);
+            SwerveLeftTurn(150);
         }else if(rightSum > leftSum){ //小车偏右
-            SwerveRightTurn(180);
+            SwerveRightTurn(150);
         }else{ 
             //小车遇到复杂情况的调整
             int prevLeft = ((lastState >> 3) & 1) + ((lastState >> 2) & 1); //左边总计
             int prevRight = ((lastState >> 1) & 1) + (lastState & 1); //右边总计
 
             if(prevLeft > prevRight){
-                SwerveLeftTurn(180);
+                SwerveLeftTurn(150);
             }else if(prevRight > prevLeft){
-                SwerveRightTurn(180);
+                SwerveRightTurn(150);
             }else{
-                SwerveForward(200);
+                SwerveForward(160);
             }
         }
     }
 
-    lastState = state;
+    if(state != 0){
+        lastState = state;
+    }
 
     delay(50);
 }
